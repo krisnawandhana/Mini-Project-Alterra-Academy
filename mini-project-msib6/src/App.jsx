8816d1d6b28ad23d3e4947c54343155e5020832c
Krisna Wandhana <warriorkrisna@gmail.com>
2024-04-07 23:04:38 +0800

    update news

--- mini-project-msib6/src/App.jsx
+++ mini-project-msib6/src/App.jsx
@@ -37,17 +37,17 @@ function App() {
           </div>
           <div className="flex gap-8 col-span-2 justify-center">
             <div className="group relative">
-              <img src="/assets/merch.png" alt="merch 1" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
+              <img src="/assets/merch2.png" alt="merch 1" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
               <div className="rounded-lg absolute inset-0 bg-gradient-to-b from-transparent to-rose-300 opacity-0 group-hover:opacity-60 scale-105 transition duration-300"></div>
               <p className="text-white absolute inset-x-0 bottom-0 px-4 py-2 invisible group-hover:visible scale-105 transition duration-300  ">Merch Description 1</p>
             </div>
             <div className="group relative">
-              <img src="/assets/merch.png" alt="merch 2" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
+              <img src="/assets/merch2.png" alt="merch 2" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
               <div className="rounded-lg absolute inset-0 bg-gradient-to-b from-transparent to-rose-300 opacity-0 group-hover:opacity-60 scale-105 transition duration-300"></div>
               <p className="text-white absolute inset-x-0 bottom-0 px-4 py-2 invisible group-hover:visible scale-105 transition duration-300">Merch Description 2</p>
             </div>
             <div className="group relative">
-              <img src="/assets/merch.png" alt="merch 3" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
+              <img src="/assets/merch2.png" alt="merch 3" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
               <div className="rounded-lg absolute inset-0 bg-gradient-to-b from-transparent to-rose-300 opacity-0 group-hover:opacity-60 scale-105 transition duration-300"></div>
               <p className="text-white absolute inset-x-0 bottom-0 px-4 py-2 invisible group-hover:visible scale-105 transition duration-300">Merch Description 3</p>
             </div>
@@ -62,8 +62,39 @@ function App() {
             <h1 className="font-black my-8 col-span-2 text-rose-300 text-xl tracking-[4px]">GALLERY</h1>
             <hr className="flex-grow border-t-4 border-rose-300 w-8" />
           </div>
+
+          <div className="flex gap-8 col-span-2 justify-center">
+            <div className="group relative">
+              <img src="/assets/galeri1.png" alt="merch 1" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
+              <p className="font-serif mt-4 flex justify-center text-xs tracking-[4px] text-[#424242]">
+                お隣の天使様にいつの間にか駄目人間に
+              </p>
+              <p className="font-normal flex justify-center text-xl text-[#424242]">
+                Integer: Joukouhougaku no Sekai
+              </p>
+            </div>
+            <div className="group relative">
+              <img src="/assets/galeri1.png" alt="merch 2" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
+              <p className="font-serif mt-4 flex justify-center text-xs tracking-[4px] text-[#424242]">
+                お隣の天使様にいつの間にか駄目人間に
+              </p>
+              <p className="font-normal flex justify-center text-xl text-[#424242]">
+                Integer: Joukouhougaku no Sekai
+              </p>
+            </div>
+            <div className="group relative">
+              <img src="/assets/galeri1.png" alt="merch 3" className="w-1/1 rounded-lg shadow-md group-hover:scale-105 transition duration-300"/>
+              <p className="font-serif mt-4 flex justify-center text-xs tracking-[4px] text-[#424242]">
+                お隣の天使様にいつの間にか駄目人間に
+              </p>
+              <p className="font-normal flex justify-center text-xl text-[#424242]">
+                Integer: Joukouhougaku no Sekai
+              </p>
+            </div>
+          </div>
+
           <br />
-          <a href="#" className="mt-4 font-sheriff my-8 text-rose-300 text-lg flex justify-end items-end">More Gallery &rarr;</a>
+          <a href="#" className="mt-4 font-serif my-8 text-rose-300 text-lg flex justify-end items-end">More Gallery &rarr;</a>
         </div>
 
         <Footer />
--- mini-project-msib6/src/pages/News.jsx
+++ mini-project-msib6/src/pages/News.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function News() {
+const News = () => {
+  const [entries, setEntries] = useState([
+    { date: "2023.12.06", category: "Cosplay", title: "Die Fröhliche 『天使の聖域』" },
+    { date: "2023.12.07", category: "Cosplay", title: "Die Fröhliche 『天使の聖域』" },
+    { date: "2023.12.07", category: "Cosplay", title: "Die Fröhliche 『天使の聖域』" }
+  ]);
+
+  const [hoveredRow, setHoveredRow] = useState(null);
+
   return (
     <div>
-        <div className="flex items-center gap-8">
-            <img src="/logo.svg" alt="logo" width={"40px"} />
-            <h1 className="font-black my-8 col-span-2 text-rose-300 text-xl tracking-[4px]">NEWS</h1>
-            <hr className="flex-grow border-t-4 border-rose-300 w-8" />
-        </div>
-        <p className="mt-4 font-medium tracking-[4px] text-md">
-            韓ワヲヌ勝43練そク中残未佐億フヤトエ峰口フス女始んれ完豪ばッめた留京ゅど般痢ヨ速更まぜどは記止らあき携汚商負坊斉遣さリば。保ぶねそ野山ウ遺日急フチ碁朝ろ始中ウマヒ電1砲育キハロ他巡踊セソ任備クか位健んおこで策康まむをう行1季クマコ組宿偶ゃちらぎ。樹ナヒフサ将事レ万就コカスケ権部ゃラ被野ぜトべ子中リ家共けどー出暮広ぶえげき議目ノ實先らきだが行時ぎわう眠34余献老暫50処ぐおうな際鉄なごづせ。
-        </p>
-        <br />
-        <a href="#" className="font-sheriff text-rose-300 text-lg flex justify-end items-end">More News &rarr;</a>
+      <div className="flex items-center gap-8">
+        <img src="/logo.svg" alt="logo" width={"40px"} />
+        <h1 className="font-black my-8 col-span-2 text-rose-300 text-xl tracking-[4px]">NEWS</h1>
+        <hr className="flex-grow border-t-4 border-rose-300 w-8" />
+      </div>
+
+      <table className="table-fixed w-full text-xl text-center text-gray-500">
+        <thead className="justify-center text-xl text-white uppercase bg-rose-300">
+          <tr>
+            <th className="col p-2">Tanggal</th>
+            <th className="col p-2">Kategori</th>
+            <th className="col p-2">Judul</th>
+          </tr>
+        </thead>
+        <tbody>
+        {entries.map((entry, index) => (
+            <tr
+              key={index}
+              className={hoveredRow === index ? "bg-rose-400 text-white" : ""}
+              onMouseEnter={() => setHoveredRow(index)}
+              onMouseLeave={() => setHoveredRow(null)}
+            >
+              <td className="p-2 text-center">{entry.date}</td>
+              <td className="p-2 text-center">{entry.category}</td>
+              <td className="p-2 text-center">{entry.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <br />
+      <a href="#" className="font-s text-rose-300 text-lg flex justify-end items-end">More News &rarr;</a>
     </div>
-  )
-}
+  );
+};
+
+export default News;
